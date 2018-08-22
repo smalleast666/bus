ba98b63988979aa26ee6254cc2aebfbb1ab671ce
smalleast <963732141@qq.com>
2018-08-22 21:41:06 +0800

    .

--- src/views/home/index.jsx
+++ src/views/home/index.jsx
@@ -10,6 +10,7 @@ class Home extends React.Component {
         super()
         this.state = {
             open: false,
+            errMsg: '',
             cityId: '003',
             lineNo: '',
             lineId: '',
@@ -17,7 +18,8 @@ class Home extends React.Component {
             buses: [],
         }
         this.stationClick = this.stationClick.bind(this);
-        this.search = this.search.bind(this);
+        this.searchClick = this.searchClick.bind(this);
+        this.searchFun = this.searchFun.bind(this);
         this.handleClose = this.handleClose.bind(this);
     }
 
@@ -32,23 +34,35 @@ class Home extends React.Component {
         });
     }
 
-    search() {
+    searchClick(e) {
+        if (e && e.keyCode) {
+            if (e.keyCode !== 13) return;
+        }
+        this.setState({
+            lineId: '',
+        }, () => this.searchFun());
+    }
+    searchFun() {
         const { lineNo, lineId, cityId } = this.state;
         if (!lineNo) {
             return this.setState({
-                open: true
-            })
+                open: true,
+            });
         }
-
         const url = `http://223.6.254.105/bus/line!lineDetail.action?lineName=${lineNo}&lineNo=${lineNo}&cityId=${cityId}&sign=signkey11&s=IOS&stats_referer=searchHistory&v=5.34.0${lineId ? '&lineId=' + lineId : ''}`;
         axios.get(url).then(res => {
-            const data = res.data.slice(6, -6);
-            const { stations, line } = JSON.parse(data).jsonr.data;
-            console.log(stations)
-
+            const data = JSON.parse(res.data.slice(6, -6)).jsonr;
+            if (data.data === {}) {
+                return this.setState({
+                    open: true,
+                    errMsg: data.errmsg
+                });
+            }
+            const { stations, line } = data.data;
             this.setState({ stations, lineId: line.lineId })
         });
     }
+
     handleClose() {
         return this.setState({
             open: false,
@@ -56,13 +70,13 @@ class Home extends React.Component {
     }
 
     render() {
-        const { stations, buses, order, open } = this.state;
+        const { stations, buses, order, open, errMsg } = this.state;
         return (
             <div>
-                重庆：<input type="text" onChange={(e) => this.setState({ lineNo: e.target.value })} />
+                重庆：<input type="text" onKeyUp={(e) => this.searchClick(e)} onChange={(e) => this.setState({ lineNo: e.target.value })} />
                 <Button
                     color="primary"
-                    onClick={() => this.search()}
+                    onClick={() => this.searchClick()}
                 >搜索
                 </Button>
 
@@ -87,7 +101,7 @@ class Home extends React.Component {
                     ContentProps={{
                         'aria-describedby': 'message-id',
                     }}
-                    message={<span>I love snacks</span>}
+                    message={<span>{errMsg}</span>}
                 />
 
             </div>
